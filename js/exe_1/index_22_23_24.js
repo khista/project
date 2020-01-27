bdfaf5610e8ff5080d55f005ba9b7295a8fbaace
fazli <khista.rahman@gmail.com>
2020-01-27 03:41:33 -0800

    add more file

--- js/exe_1/index_22_23_24.js
+++ js/exe_1/index_22_23_24.js
@@ -13,3 +13,21 @@ let logged =false;
 let likesJS =true;
 let tamplate =`Done : ${done} \nMarried: ${married} \nLogged: ${logged} \nLikes JS:${likesJS}`;
 console.log(tamplate);
+
+//exe_23
+/* Create a new index23.js file
+Define a username variable
+Show the username variable value using console.log() with the variable name (string) as first parameter and the actual variable value as second
+Example: console.log('name', name); */
+let username;
+console.log('Name: '+username);
+//exe_24
+/* Create a new index24.js file
+Define a username variable and assign a null value
+Show the username variable value using console.log() with the variable name (string) as first parameter and the actual variable value as second
+Example: console.log('name', name); */
+
+let username1 =null;
+
+console.log('user name : ',username1);
+
