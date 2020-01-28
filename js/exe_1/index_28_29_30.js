2efd0dce912e4da9c44d817d89053f8ecef764dc
fazli <khista.rahman@gmail.com>
2020-01-27 19:14:01 -0800

    add more exe_28'

---
+++ js/exe_1/index_28_29_30.js
@@ -0,0 +1,15 @@
+//exe_28
+/* 
+Create a new index28.js file
+Define a number variabe and assign the following value: 9
+Using the number variable show the multiplication table from 1 to 10
+Example: 9 times 1 equals 9
+Use string concatenation for each output and console.log()
+ */
+
+let num1 =1;
+ for(let x =0;x<=10;x++){
+     console.log('Number is :',x +'multiplay by ',num1+ ' result is =  ',num1 * x);
+ }
+
+ //exe_29
