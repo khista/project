8b6a776e202def7f4aff1676fb258cff92e34a6a
fazli <khista.rahman@gmail.com>
2020-02-04 00:13:55 -0800

    add more exe

--- js/exe_1/index_46_47_48_49_50.js
+++ js/exe_1/index_46_47_48_49_50.js
@@ -7,21 +7,21 @@ Show the following output: The user %username% has %password% as password
 
 */
 
-let userAndPassword ='pepito2017,12345';
+/* let userAndPassword ='pepito2017,12345';
 let username = userAndPassword.substr(0,10);
 let password = userAndPassword.substr(11,20);
 console.log("User Name: "+username);
 console.log("Password is : "+password);
 
 console.log(`The user: ${username} has password: ${password}`);
-
+ */
 //exe_47
 /* Define a students variable and assign 5 student names from your class, separated by commas
 Define a result variable and use the students variable split method to get a students name array and assign it as result variable value
 Show the result value as output: console.log('Students names:', %result%) (something like this) */
 
 
-const students = "waleed khibar kabir jamil osman";
+/* const students = "waleed khibar kabir jamil osman";
 let singalName = students.split(' ');//split each name in full word.
 console.log("Students names: "+singalName[2]);// the number is for index 
 let char = students.split('');// return the char at index 
@@ -36,7 +36,7 @@ console.log(parCopy);
 let longString1 = "This is a very long string which needs \
 to wrap across multiple lines because \
 otherwise my code is unreadable.";
-console.log(longString1);
+console.log(longString1); */
 
 //exe_48
 /* 
@@ -49,6 +49,22 @@ Show the following output: 45.14 is the result of adding pi and answerToLife
 Concatenate the strings and transform 45.14 (result value) from number to string using toString method
 
 */
+let messages1 = '3.14 it\'s a great number but 42 it\'s the answer to life';
+ 
+let pi = parseFloat(messages1.slice(0,5));
+console.log(typeof pi + pi);
+
+console.log(typeof messages1.substr(0))
+ 
+let answerToLife = parseInt(messages1.slice(29,31));
+console.log(typeof answerToLife +' '+answerToLife );
+ 
+let result = pi + answerToLife;
+
+console.log(result);
+
+let conv = result.toString();
+console.log(result);
 
 
 
@@ -61,3 +77,35 @@ console.log("Type of part: "+typeof ConvToNum +" \nresult : "+ConvToNum );
 
 let x = 6;
 console.log(x.toString() +'23');
+ console.log("----------------------49-----------------------------");
+//exe_49
+/* 
+Define 2 variables with the names firstNumber and secondNumber
+Assign firstNumber the number 10
+Assign secondNumber the number 5
+If the first number is bigger than the second number then show the following output: Number %firstNumber% is bigger than %secondNumber%
+Now try changing the secondNumber value and see what happens when you execute the code
+*/
+let firstNum =10;
+let secondNum =20;
+if(firstNum>secondNum){
+    console.log(`The first namber ${firstNum} is bigger than ${secondNum}`);
+}else{
+    console.log(`The second Number ${secondNum} is bigger than ${firstNum}`);
+}
+
+
+//exe_50
+
+/* Define a number variable and assign the following value: 10
+If number is even then show the following message: %number% is an even number
+Try changing the value to an odd number to see what happens */
+
+
+let anum = 50;
+if(anum %2 ==0){
+    console.log(` ${anum} is an even number`);
+
+}else{
+    console.log(`${anum} is not an even number`);
+}
---
+++ js/exe_1/priactice.js
@@ -0,0 +1,17 @@
+var a = 8;
+var b =9;
+
+function swap(a,b){
+    let temp =a;
+    a=b;
+    b =temp;
+      return ` The value a : ${a}  and the value b: ${b}`;
+}
+
+
+
+
+console.log(`The value ${b} and the value ${a} \n`);
+
+ let swa=swap(b,a);
+console.log(swa);
