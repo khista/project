a2318b47e6c66d3dbdd98ecebd304b5cac6222a9
fazli <khista.rahman@gmail.com>
2020-02-07 17:41:58 -0800

    add more exe

--- js/exe_1/index100.js
+++ js/exe_1/index100.js
@@ -5,9 +5,20 @@ The iteration must break after finding the first 20 even numbers
 Show the result as output
 */
 
-for(let index =0; index<100;index++){
+/* for(let index =0; index<100;index++){
     if(index % 2 == 0){
         console.log('index '+index);
     }
     
+} */
+
+/* 
+Define a function that will show even numbers from 0 to 100 as output
+*/
+
+function numbers(){
+    for(let index =0;index<100;index++){
+        console.log(index);
+    }
 }
+console.log(numbers());
