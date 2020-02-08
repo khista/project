f111c4775de95320e018010e66885bfbda46e6e2
fazli <khista.rahman@gmail.com>
2020-02-07 19:08:30 -0800

    add exe 102'

--- js/exe_1/index102.js
+++ js/exe_1/index102.js
@@ -8,31 +8,17 @@ Call the showName at least 2 times */
 
 function names(){
     let heading = '';
-    /* for(let index1 =1; index1<=5;index1++){
-        for(let index2=1;index2<=12;index2++){
-            if(index1==2 && index2==1){
-                console.log('\n')
-            }else if(index1=4 && index2==1){
-                 console.log(' ')
-            }else if(index1==3 && index2==2){
-                console.log('ComIT');
-            }else
-            console.log('=');
-             
-        }
-    } */
-    for(let index1 =1; index1<=3;index1++){
+    //console.log(heading);
+      for(let index1 =1; index1<=3;index1++){
              if(index1==2){
                 console.log('= ComIT =');
             }else {
-                if(heading){
                     heading ='';
-                }
-                for(let index2=1;index2<=5;index2++){
+                for(let index2=1;index2<=9;index2++){
                     heading += '=';
                 }
                 console.log(heading);
             }
-    }
+    }  
 }
 console.log(names());
