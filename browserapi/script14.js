4c1a4c0a7fcae10e0cf99c1e56fdd2c9e4496f6e
fazli <khista.rahman@gmail.com>
2020-02-14 04:41:55 -0800

    add more api exes

---
+++ browserapi/script14.js
@@ -0,0 +1,20 @@
+/* Create a basic HTML document
+Create a script tag on the document head element
+Show the user an alert dialog with the following message once the document is loaded
+The Browser has finisedh loading, parsing and downloading all related elements, Time To Rock!!! */
+
+   window.onload = function(){
+    window.alert(`Time To Rock!!!`);
+}; 
+
+/* let div = document.querySelector('div');
+let span = document.querySelector('span');
+
+console.log(div);
+console.log(span); */
+
+  //let div = document.querySelectorAll(".timepassed");
+  //span = document.querySelector('span.red');
+
+  //console.log(div);
+//console.log(span);
