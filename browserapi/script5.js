d9f18ba2bb8d5fdc8e6a0f0697ead9b89eced75b
fazli <khista.rahman@gmail.com>
2020-02-14 18:17:41 -0800

    add 5 exe api

--- browserapi/script5.js
+++ browserapi/script5.js
@@ -3,7 +3,7 @@ Create a script tag on the document head element
 Using the prompt method ask the user to input a number
 Using the alert method show if the number is even or odd
 Your number is even/odd */
-
+//
 
 let number1 = parseInt( window.prompt("Enter Number and i will tell is even/odd"));
  
