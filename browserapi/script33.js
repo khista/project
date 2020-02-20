633df4abb9ff75200f472a639d8e440d0020d08f
fazli <khista.rahman@gmail.com>
2020-02-20 00:39:52 -0800

    exes api

--- browserapi/script33.js
+++ browserapi/script33.js
@@ -9,7 +9,13 @@
 
 
 
+let febanacci = function(n){
+    if(n<2) return n;
+    return febanacci(n-1) + febanacci(n-2);
+}
+let postion = 10;
 
+console.log(febanacci(postion));
 
 
 /* let button = document.querySelector('button');
